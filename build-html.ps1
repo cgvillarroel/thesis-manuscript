b9163ff8d7b0bf103e4fffe893e5778f67339502
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2024-06-03 20:15:13 +0800

    docs: html support

--- build-html.ps1
+++ build-html.ps1
@@ -2,4 +2,5 @@ write-host -nonewline "Converting to HTML... `t"
 new-item -type directory output -erroraction ignore > $null
 remove-item output/manuscript.pdf -erroraction ignore > $null
 asciidoctor -r asciidoctor-bibtex -b html5 src/manuscript.adoc -o output/manuscript.html
-write-host "Done"
+write-host "Done`n"
+write-host "Done building. See ./output/manuscript.html"
