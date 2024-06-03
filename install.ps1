1f25a5c16533b1901f8d3f55a98df750c7de3e0f
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2024-06-03 15:05:56 +0800

    install script shenanigans

--- install.ps1
+++ install.ps1
@@ -4,7 +4,6 @@ write-host "Checking if Scoop is installed... `t"
 scoop --version
 if (-not $?) {
 	write-host "Scoop is not installed. Installing..."
-	Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 	Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 	write-host "Done"
 }
@@ -14,21 +13,20 @@ write-host "Scoop is installed."
 write-host "Installing dependencies from Scoop..."
 scoop install aria2
 scoop config aria2-warning-enabled false
-scoop install pandoc pdftk msys2
-scoop install ruby
-scoop install miktex
+scoop install pandoc pdftk miktex msys2 ruby
 write-host "Installed dependencies from Scoop."
 
 # INSTALL RUBY DEPENDENCIES
 write-host "Installing Ruby dependencies..."
+msys2
 ridk install 3
+ridk enable
 write-host "Installed Ruby dependencies."
 
 # INSTALL ASCIIDOCTOR & EXTENSION
 write-host "Installing Asciidoctor and extension..."
-gem install asciidoctor
-gem install asciidoctor-bibtex
-write-host "Installed Asciidoctor and extension."
+gem install asciidoctor asciidoctor-bibtex
+write-host "Installed Asciidoctor and extension.`n"
 
 write-host "Finished installing dependencies."
 write-host "Please update MiKTeX through the MiKTeX Console."
